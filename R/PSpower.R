60abc551422e3661b810140caa49fdc3149dd7e2
Bo Liu <bo@MacBook-Pro-2.local>
2025-09-25 00:18:03 -0400

    update

--- R/PSpower.R
+++ R/PSpower.R
@@ -1,30 +1,23 @@
 #' Calculate sample size needed to achieve a prespecified power
 #'
 #' @importFrom stats qnorm
-#' @param tau the estimated treatment effect $E[Y(1) - Y(0)]$
-#' @param alpha the significance level
-#' @param beta the power to achieve
+#' @importFrom stats pnorm
+#' @param tau the anticipated standardized treatment effect
+#' @param sig.level the significance level, or the type-I error rate (default 0.05)
+#' @param power the desired power to achieve (only specify for sample size calculation)
+#' @param sample.size the total sample size (only specify for power calculation)
 #' @param r the proportion of treated units
-#' @param phi the overlap coefficients
-#' @param E1,E0,S1,S0,R1,R0 the summary quantities
+#' @param phi the overlap coefficient (usually between 0.8 and 1); use function plot_overlap(r, phi) for visual aid
+#' @param rho_sq the squared correlation between propensity score and outcome; recommend treating as a sensitivity parameter: a grid of values between 0 and the R-squared statistic of predicting the outcomes with covariates.
 #' @param test whether one-sided or two-sided test is considered
-#' @param estimand the estimand (ATE, ATT, ATC or ATO), or a customized tilting function
+#' @param estimand the estimand (ATE, ATT, ATC or ATO), or a customized tilting function h(e(x))
 #' @returns an object with the calculated sample size
 #' @examples
-#' PSpower(1, 0.05, 0.956, 0.5, 0.99, -1.74, -2.74, 19.86, 20.12, 0.14, 0.14)
+#' PSpower(tau = 1/sqrt(20), sig.level = 0.05, power = 0.956, r = 0.5, phi = 0.99, rho_sq = 0.02)
 #' @export
-PSpower <- function(tau, alpha, beta, r, phi, E1, E0, S1, S0, R1, R0,
+PSpower <- function(tau, sig.level = 0.05, power = NULL, sample.size = NULL, r, phi, rho_sq,
                     test = 'two-sided', estimand = 'ATE') {
-  params <- solve_parameters(r, phi, E1, E0, S1, S0, R1, R0)
-  if (test == 'two-sided') {
-    coef <- (qnorm(1 - alpha / 2) + qnorm(beta))^2
-  }
-  else if (test == 'one-sided') {
-    coef <- (qnorm(1 - alpha) + qnorm(beta))^2
-  }
-  else {
-    stop('test should be either one-sided or two-sided.')
-  }
+  params <- solve_parameters(r, phi, rho_sq)
   if (typeof(estimand) == 'character') {
     if(!estimand %in% c('ATE', 'ATT', 'ATC', 'ATO'))
       stop('estimand should be one of ATE, ATT, ATC and ATO, or the tilting function.')
@@ -39,12 +32,43 @@ PSpower <- function(tau, alpha, beta, r, phi, E1, E0, S1, S0, R1, R0,
   else {
     stop('estimand should be one of ATE, ATT, ATC and ATO, or the tilting function.')
   }
+
+  if (!is.null(power)) {
+    if (test == 'two-sided') {
+      coef <- (qnorm(1 - sig.level / 2) + qnorm(power))^2
+    }
+    else if (test == 'one-sided') {
+      coef <- (qnorm(1 - sig.level) + qnorm(power))^2
+    }
+    else {
+      stop('test should be either one-sided or two-sided.')
+    }
+    sample_size <- coef * V / tau^2
+    power <- power
+    ss_calculation <- TRUE
+  }
+  else {
+    if (test == 'two-sided') {
+      power <- pnorm(sqrt(sample.size / V) * tau - qnorm(1 - sig.level / 2))
+    }
+    else if (test == 'one-sided') {
+      power <- pnorm(sqrt(sample.size / V) * tau - qnorm(1 - sig.level))
+    }
+    else {
+      stop('test should be either one-sided or two-sided.')
+    }
+    sample_size <- sample.size
+    power <- power
+    ss_calculation <- FALSE
+  }
+
   return (structure(
     list(
-      sample_size = coef * V / tau^2,
-      tau = tau, alpha = alpha, beta = beta, test = test, estimand = estimand,
-      summaries = c(r = r, phi = phi, E1 = E1, E0 = E0, S1 = S1, S0 = S0, R1 = R1, R0 = R0),
-      params = params
+      sample_size = sample_size,
+      tau = tau, sig.level = sig.level, power = power, test = test, estimand = estimand,
+      summaries = c(r = r, phi = phi, rho_sq = rho_sq),
+      params = params,
+      ss_calculation = ss_calculation
     ),
     class = 'PSpower'
   ))
@@ -53,9 +77,15 @@ PSpower <- function(tau, alpha, beta, r, phi, E1, E0, S1, S0, R1, R0,
 #' Prints PSpower object
 #' @param x PSpower object
 #' @param ... ignored
+#' @returns no return value; called for side effect to output a string
 #' @export
 print.PSpower <- function(x, ...) {
-  cat('Estimated sample size:', x$sample_size, '\n')
+  if (x$ss_calculation) {
+    cat('Estimated sample size to achieve', round(x$power, 3), 'power:', round(x$sample_size), '\n')
+  }
+  else {
+    cat('Calculated power at sample size', round(x$sample_size), 'is', round(x$power, 3), '\n')
+  }
 }
 
 #' Plots PSpower object
@@ -67,17 +97,18 @@ print.PSpower <- function(x, ...) {
 #' @importFrom ggplot2 geom_point
 #' @importFrom ggplot2 geom_line
 #' @examples
-#' obj <- PSpower(1, 0.05, 0.956, 0.5, 0.99, -1.74, -2.74, 19.86, 20.12, 0.14, 0.14)
+#' obj <- PSpower(tau = 1/sqrt(20), sig.level = 0.05, power = 0.956,
+#'                r = 0.5, phi = 0.99, rho_sq = 0.02)
 #' plot(obj)
+#' @returns an object (class ggplot) containing a figure
 #' @export
 plot.PSpower <- function(x, power = seq(0.6, 0.99, length.out = 100), ...) {
   size <- sapply(power,
          function(p)
-           PSpower(x$tau, x$alpha, p, x$summaries['r'], x$summaries['phi'],
-                   x$summaries['E1'], x$summaries['E0'], x$summaries['S1'], x$summaries['S0'],
-                   x$summaries['R1'], x$summaries['R0'], x$test, x$estimand)$sample_size
+           PSpower(x$tau, x$sig.level, p, x$summaries['r'], x$summaries['phi'],
+                   x$summaries['rho_sq'], sample.size = NULL, x$test, x$estimand)$sample_size
   )
   ggplot2::ggplot(data.frame(power = power, size = size), ggplot2::aes(size, power)) +
     ggplot2::geom_line() +
-    ggplot2::geom_point(data = data.frame(power = x$beta, size = x$sample_size))
+    ggplot2::geom_point(data = data.frame(power = x$power, size = x$sample_size))
 }
--- R/util.R
+++ R/util.R
@@ -1,5 +1,10 @@
 expit <- function(x) ifelse(x > 0, 1 / (1 + exp(-x)), exp(x) / (1 + exp(x)))
 log_expit <- function(x) ifelse(x > 0, -log1p(exp(-x)), x - log1p(exp(x)))
+logit <- function(x) log(x) - log(1 - x)
+
+dlogitnorm <- function(x, mu, sigma) {
+  1 / (sigma * sqrt(2*pi) * x * (1 - x)) * exp(-(logit(x) - mu)^2 / (2 * sigma^2))
+}
 
 overlap <- function(r, k) {
   alpha <- k * r
@@ -13,9 +18,7 @@ overlap <- function(r, k) {
   ))
 }
 
-#' @importFrom stats dnorm
-#' @importFrom stats integrate
-solve_parameters <- function(r, phi, E1, E0, S1, S0, R1, R0) {
+solve_overlap <- function(r, phi) {
   f_overlap <- function(k) {
     overlap(r, k)$log_overlap - log(phi)
   }
@@ -27,7 +30,14 @@ solve_parameters <- function(r, phi, E1, E0, S1, S0, R1, R0) {
     if (f_overlap(M) < 0) {L <- M}
     else {R <- M}
   }
-  overlap_object <- overlap(r, M)
+  return (overlap(r, M))
+}
+
+#' @importFrom stats dnorm
+#' @importFrom stats integrate
+solve_parameters <- function(r, phi, rho_sq) {
+
+  overlap_object <- solve_overlap(r, phi)
   mu_e <- overlap_object$mu
   sigma_sq_e <- overlap_object$sigma^2
 
@@ -44,21 +54,12 @@ solve_parameters <- function(r, phi, E1, E0, S1, S0, R1, R0) {
 
   E1_e <- I11 / I01
   V1_e <- I21 / I01 - E1_e^2
-  a_1 <- R1 * sqrt(S1 / V1_e)
-  mu_1 <- E1 - a_1 * E1_e
-  sigma_sq_1 <- (1 - R1^2) * S1
+  a_1_sq <- rho_sq / V1_e
+  sigma_sq <- 1 - rho_sq
 
   E0_e <- I10 / I00
   V0_e <- I20 / I00 - E0_e^2
-  a_0 <- R0 * sqrt(S0 / V0_e)
-  mu_0 <- E0 - a_0 * E0_e
-  sigma_sq_0 <- (1 - R0^2) * S0
-
-  parameters <- c(
-    mu_e = mu_e, sigma_sq_e = sigma_sq_e,
-    a_1 = a_1, a_0 = a_0, mu_1 = mu_1, mu_0 = mu_0,
-    sigma_sq_1 = sigma_sq_1, sigma_sq_0 = sigma_sq_0
-  )
+  a_0_sq <- rho_sq / V0_e
 
   calc_V <- function(h) {
     E_We_h <- integrate(function(x) x * h(expit(x)) * dnorm(x, mu_e, sqrt(sigma_sq_e)),
@@ -87,14 +88,14 @@ solve_parameters <- function(r, phi, E1, E0, S1, S0, R1, R0) {
       function(x) h(expit(x))^2 * dnorm(x, mu_e, sqrt(sigma_sq_e)),
       lower = -Inf, upper = Inf
     )$value
-    return ((a_1^2 * term1 + a_0^2 * term0 + sigma_sq_0 * termn0 + sigma_sq_1 * termn1) / denom)
+    return ((a_1_sq * term1 + a_0_sq * term0 + sigma_sq * termn0 + sigma_sq * termn1) / denom)
   }
 
   return (list(
     parameters = c(
       mu_e = mu_e, sigma_sq_e = sigma_sq_e,
-      a_1 = a_1, a_0 = a_0, mu_1 = mu_1, mu_0 = mu_0,
-      sigma_sq_1 = sigma_sq_1, sigma_sq_0 = sigma_sq_0
+      a_1_sq = a_1_sq, a_0_sq = a_0_sq,
+      sigma_sq = sigma_sq
     ),
     V_ATE = calc_V(function(x) 1),
     V_ATT = calc_V(function(x) x),
